67130b5f993625d77662e22a93627cd7018df57d
VineetKT <vineetiitk95@gmail.com>
2021-07-25 11:41:49 +0530

    lintered py and aws creds

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ test the github actions
 def add(x, y):
     return x+y
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     print(add(3, 4))
--- test_main.py
+++ test_main.py
@@ -4,5 +4,6 @@ pytest module for main.py
 
 from main import add
 
+
 def test_main():
-    assert add(5,7)==12
+    assert add(5, 7) == 12
