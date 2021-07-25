7c7b9f6995b8daab0587805537eeb23a759f87db
VineetKT <vineetiitk95@gmail.com>
2021-07-25 10:43:22 +0530

    added dummy files to test github actions

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+test the github actions
+"""
+
+
+def add(x, y):
+    return x+y
+
+if __name__=='__main__':
+    print(add(3, 4))
---
+++ test_main.py
@@ -0,0 +1,8 @@
+"""
+pytest module for main.py
+"""
+
+from main import add
+
+def test_main():
+    assert add(5,7)==12
