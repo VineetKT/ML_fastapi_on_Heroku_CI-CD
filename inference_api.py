534c28a92d339b4fc19c039611cd1621a6f3cf48
VineetKT <vineetiitk95@gmail.com>
2021-07-27 22:34:52 +0530

    changed dvc pull args on heroku

--- inference_api.py
+++ inference_api.py
@@ -39,10 +39,11 @@ class RowData(BaseModel):
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
+    os.system('rm -Rf .dvc/cache')
     # os.system("dvc pull")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    os.system("rm -Rf .dvc .apt/usr/lib/dvc")
 
 
 @app.get("/")
