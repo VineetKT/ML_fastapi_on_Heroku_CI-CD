d86a0d1e8392ce981e7d0f2cf5c58194a5e74c0c
VineetKT <vineetiitk95@gmail.com>
2021-07-27 22:24:25 +0530

    dvc pull added as per md file

--- inference_api.py
+++ inference_api.py
@@ -39,9 +39,9 @@ class RowData(BaseModel):
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    os.system("dvc pull")
-    # if os.system("dvc pull") != 0:
-        # exit("dvc pull failed")
+    # os.system("dvc pull")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
